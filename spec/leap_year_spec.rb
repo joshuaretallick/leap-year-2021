efd25cc67c1b7de3de3ee71a7be7db6d2edf63ea
Joshua Retallick <josh@old-empire.co.uk>
2021-01-13 10:38:33 +0000

    Added extreme examples to check vulnerability in code

--- spec/leap_year_spec.rb
+++ spec/leap_year_spec.rb
@@ -18,4 +18,16 @@ describe "#leapyear method" do
     expect(leapyear(6)).to eq false
   end
 
+  it "returns true when leapyear is divisible by 4 but NOT by 100" do
+    expect(leapyear(804)).to eq true
+  end
+
+  it "returns true when leapyear is divisible by 4 but NOT by 100" do
+    expect(leapyear(92504)).to eq true
+  end
+
+  it "returns false when leapyear is NOT divisible by 4" do
+    expect(leapyear(3567098766909)).to eq false
+  end
+
 end
